8ea86efbd5f1fd9d396a676a64260ba4936b0962
Egor <laptenauck@gmail.com>
2021-01-21 22:03:44 +0300

    add navigation header; change theme; add season picker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,20 @@ import { SeasonsComponent } from './seasons/seasons.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SeasonsComponent
+    SeasonsComponent,
+    MainNavComponent
   ],
   imports: [
     BrowserModule,
@@ -22,7 +31,14 @@ import { MatSortModule } from '@angular/material/sort';
     BrowserAnimationsModule,
     MatTableModule,
     MatPaginatorModule,
-    MatSortModule
+    MatSortModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
