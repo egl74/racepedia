d6d76461eec4db58b90f9f2be191f03f4e6822f4
Egor <laptenauck@gmail.com>
2021-01-26 20:35:21 +0300

    add emoji flags

--- src/app/season/season-round-table/season-round-table.component.ts
+++ src/app/season/season-round-table/season-round-table.component.ts
@@ -1,11 +1,12 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatTable } from '@angular/material/table';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { of } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { RaceModel } from '../../models/race.model';
 import { SeasonService } from '../../services/season.service';
-import { SeasonRoundTableDataSource } from './season-round-table-datasource'
+import { SeasonRoundTableDataSource } from './season-round-table-datasource';
+const { flag } = require('country-emoji');
 
 @Component({
   selector: 'app-season-round-table',
@@ -14,18 +15,19 @@ import { SeasonRoundTableDataSource } from './season-round-table-datasource'
 })
 export class SeasonRoundTableComponent implements AfterViewInit {
   @ViewChild(MatTable) table: MatTable<RaceModel>;
-  
+
   dataSource: SeasonRoundTableDataSource;
+  flag = flag;
 
   constructor(
     private readonly seasonService: SeasonService,
-    private readonly router: Router,
     private readonly route: ActivatedRoute
   ) {
     this.dataSource = new SeasonRoundTableDataSource(
       this.seasonService,
-      this.route.parent ? this.route.parent.params.pipe(
-        map((params) => params['season'])) : of()
+      this.route.parent
+        ? this.route.parent.params.pipe(map((params) => params['season']))
+        : of()
     );
   }
 
